142eeb0bf1c8f2b34186622b9e2c42af61a9257e
g.kuzm4 <job.kuzm@yandex.ru>
2019-09-15 00:24:55 +0700

    Добвалены сериализация и десериализация

---
+++ src/FileReaderWriter.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class FileReaderWriter {
+
+    public static void writeFile(final Storage carStorage) throws IOException {
+        FileOutputStream outputStream = new FileOutputStream("resources/car.ser");
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+        objectOutputStream.writeObject(carStorage);
+        objectOutputStream.close();
+    }
+
+    public static Storage readFile() throws IOException, ClassNotFoundException {
+        FileInputStream fileInputStream = new FileInputStream("resources/car.ser");
+        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+
+        Storage carStorage = (Storage) objectInputStream.readObject();
+        objectInputStream.close();
+
+        return carStorage;
+    }
+}
