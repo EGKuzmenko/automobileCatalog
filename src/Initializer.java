263d0e5df1d198d660073b9d828eaa9991b54440
g.kuzm4 <job.kuzm@yandex.ru>
2019-09-15 01:14:42 +0700

    Создан класс Initializer

---
+++ src/Initializer.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.IOException;
+
+public class Initializer {
+
+    public static void checkFile() {
+        if (!(new File("resources/car.ser").exists()))
+            init();
+    }
+
+    private static void init() {
+        Storage carStorage = new Storage();
+
+        carStorage.addCar(new Car("2010", "Suzuki", "Grand Vitara", "SUV"));
+        carStorage.addCar(new Car("2008", "Tayota", "Camry", "Sedan"));
+        carStorage.addCar(new Car("2019", "Tesla", "Model Y", "Crossover"));
+
+        try {
+            FileReaderWriter.writeFile(carStorage);
+        } catch (IOException e) {}
+    }
+
+}
